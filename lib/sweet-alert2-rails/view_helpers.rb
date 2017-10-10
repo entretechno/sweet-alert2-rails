3f6dc28991ab90bb46a3210806168ea2eaf29225
Jeremiah Megel <jeremiah@entretechno.com>
2017-10-10 09:45:18 -0500

    Add helper for button_to

--- lib/sweet-alert2-rails/view_helpers.rb
+++ lib/sweet-alert2-rails/view_helpers.rb
@@ -5,6 +5,17 @@ module SweetAlert2Rails
 
       if options_has_confirm?(html_options)
         html_options['data-sweet-alert-confirm'] = html_options.delete(:confirm) ||
+          html_options[:data].delete(:confirm)
+      end
+
+      super *args, &block
+    end
+
+    def button_to(*args, &block)
+      html_options = args[block_given? ? 1 : 2] || {}
+
+      if options_has_confirm?(html_options)
+        html_options['data-sweet-alert-confirm'] = html_options.delete(:confirm) ||
                     html_options[:data].delete(:confirm)
       end
 
